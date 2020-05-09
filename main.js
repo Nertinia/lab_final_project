4336b5bea146f0ae745d34694bc2cb5a43bad8d6
Nertinia <60277348+Nertinia@users.noreply.github.com>
2020-05-09 23:58:35 +0800

    a

---
+++ main.js
@@ -0,0 +1,24 @@
+function d1(){
+  document.getElementById('p1').style.display="flex";
+}
+function p1(){
+  document.getElementById('p1').style.display="none";
+}
+function d2(){
+  document.getElementById('p2').style.display="flex";
+}
+function p2(){
+  document.getElementById('p2').style.display="none";
+}
+function d3(){
+  document.getElementById('p3').style.display="flex";
+}
+function p3(){
+  document.getElementById('p3').style.display="none";
+}
+function d4(){
+  document.getElementById('p4').style.display="flex";
+}
+function p4(){
+  document.getElementById('p4').style.display="none";
+}
